ccfa21b4513b769d82056c3b1e48defddb945192
Mesrobyan77 <xachik.mesrobyan@inbox.ru>
2025-05-11 22:41:54 +0400

    minor changes

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -2,7 +2,7 @@ import Quiz from "../../components/Quiz/Quiz";
 
 const QuizPage = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 flex  justify-center">
       <Quiz />
     </div>
   );
